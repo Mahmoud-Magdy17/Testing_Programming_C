2e651cebbd9b26d61e8b0fad1b062a97d872e99d
Mahmoud Magdy <103686429+Mahmoud-Magdy17@users.noreply.github.com>
2023-07-12 20:56:29 +0300

    Adding problem G

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include "stack"
 using namespace std;
-void function(){
-    cout<<"Printing from the function"<<endl;
+#define ll long long
+#define endl cout<<'\n';
+void testCase(){
+    int N;cin>>N;
+    stack<int>st;
+    for(int i=0;i<N;i++){
+        int x;cin>>x;
+        st.push(x);
+    }
+    int cur=st.top();st.pop();
+    int cnt=1;
+    while(!st.empty()){
+        if(st.top()>cur){
+            cout<<cur;endl
+            cnt++;
+        }
+        cur=st.top();
+        st.pop();
+    }
+    cout<<cnt;
+}
+void testCase(int T){
+    while(T--){
+        testCase();endl
+    }
 }
 int main() {
-    function();
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    testCase();
     return 0;
 }
